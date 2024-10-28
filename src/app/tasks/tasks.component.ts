2fa46a8ef5c570068825fe096e95692dc1feeeb3
Vinicius Sempkoski <vsempkoski@gmail.com>
2024-10-28 20:31:19 -0300

    https://github.com/vinicius-sempkoski/TaskTracker.git

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {TaskComponent} from './task/task.component';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [
+    TaskComponent
+  ],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input()
+  public name!: string;
+}
