14b0da9eb7cfdb482eb129659219376a68d00ebe
Vinicius Sempkoski <vsempkoski@gmail.com>
2024-10-28 21:24:41 -0300

    https://github.com/vinicius-sempkoski/TaskTracker.git

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -11,6 +11,37 @@ import {TaskComponent} from './task/task.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input()
-  public name!: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) id!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Dominar Angular',
+      summary:
+        'Aprender Angular para poder desenvolver aplicações web',
+      dueDate: '31/11/2024',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Construir um protótipo do site da loja online',
+      summary: 'Construir um protótipo do site da loja online para a loja de roupas',
+      dueDate: '15/12/2024',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Preparar e descrever um modelo de problema',
+      summary:
+        'Preparar e descrever um modelo de problema para a disciplina de matemática',
+      dueDate: '30/1/2025',
+    },
+  ];
+  public get selectedUserTasks(){
+    return this.tasks.filter(task => task.userId === this.id);
+  }
 }
+
+
